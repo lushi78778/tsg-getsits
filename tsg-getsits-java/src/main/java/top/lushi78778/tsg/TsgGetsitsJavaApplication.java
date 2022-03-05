7ff05b2537c91473c7f22be0df7c808578a9b8c4
lushi <lushi78778@126.com>
2022-03-05 16:19:10 +0800

    初始化项目

---
+++ tsg-getsits-java/src/main/java/top/lushi78778/tsg/TsgGetsitsJavaApplication.java
@@ -0,0 +1,13 @@
+package top.lushi78778.tsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsgGetsitsJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsgGetsitsJavaApplication.class, args);
+    }
+
+}
---
+++ tsg-getsits-java/src/test/java/top/lushi78778/tsg/TsgGetsitsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lushi78778.tsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsgGetsitsJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
